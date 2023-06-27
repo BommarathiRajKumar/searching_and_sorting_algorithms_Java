9bbfe4315748189c253b106d1bda4c2bcda1f66f
Raj Kumar <rajbommarathi999@gmail.com>
2023-06-27 15:24:00 +0530

    linearSearch by using recursive approch

--- searchingAlgorithms/LinearSearch.java
+++ searchingAlgorithms/LinearSearch.java
@@ -1,15 +1,14 @@
+/*Complexity Analysis of Linear Search:
+    Time Complexity:
+    • Best Case: In the best case, the key might be present at the first index. 
+        So the best case complexity is O(1).
+    • Worst Case: In the worst case, the key might be present at the last index i.e.
+        So the worst case complexity is O(N) where N is the size of the list.
+    • Average Case: O(N)
+    • Auxiliary Space: O(1) as except the variable to iterate through the list, no other variable is used.
+*/
 public class LinearSearch {
-    /*Complexity Analysis of Linear Search:
-        Time Complexity:
-        • Best Case: In the best case, the key might be present at the first index. 
-          So the best case complexity is O(1).
-        • Worst Case: In the worst case, the key might be present at the last index i.e.
-          So the worst case complexity is O(N) where N is the size of the list.
-        • Average Case: O(N)
-        • Auxiliary Space: O(1) as except the variable to iterate through the list, no other variable is used.
-    */
-
-    public static int linearSearch(int arr[], int element){
+        public static int linearSearch(int arr[], int element){
         int len=arr.length;
         for(int i=0; i<len; i++){
             if(arr[i]==element)return i;
